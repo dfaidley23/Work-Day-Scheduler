e0850b1358d9f207acddafb23c282647dcf9c961
David faidley <david.faidley23@gmail.com>
2022-01-31 17:44:41 -0700

    added comments to my code base. Final code for project has been pushed in one update once it was tested and working locally

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-// The following line will dictate the time being displayed to the page. I used a setInterval method to fetch the current time and update it on the pageevery second.
+// The following line will dictate the time being displayed to the page. I used a setInterval method to fetch the current time and update it on the page every second.
 setInterval(() => {
   var currentDay = moment().format("dddd LL  HH:mm:ss:a");
   $("#currentDay").text(currentDay);
 }, 1000);
 
+// A variable needed to fetch the time and use it for the next function to assign the color class for the current, past, and future projects time slot.
 var now = new Date().getHours();
 
+// The main dynamic function built to take the integers from the HTML and the classes from CSS and apply the colors and required functionality to all of the App in a single function.
 function colorAndLocalStorage() {
   for (let i = 9; i < 18; i++) {
     if (now > i) {
@@ -20,8 +22,10 @@ function colorAndLocalStorage() {
   }
 }
 
+// A dynamic function that makes the Save button save the Key and Value for all of the Save icons within the App
 function button(arg) {
   localStorage.setItem(arg, $(`#projectTime${arg}`).val());
 }
 
+// A call to the function
 colorAndLocalStorage();
